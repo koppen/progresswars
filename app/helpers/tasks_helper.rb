d70e9cc15c7ead250bcef8da342a90c5f33ebe62
Jakob Skjerning <jakob@mentalized.net>
2010-03-15 09:10:37 +0100

    Visual tweaks

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -8,7 +8,10 @@ module TasksHelper
     [
       'Watching progress bars change has never been this much fun',
       'The ultimate game of being better than all your friends at making progress bars change',
-      'Countless hours of "fun"'
+      'Countless hours of fun',
+      'Just one more click...',
+      'No mafias, no vikings, no pirates - just pure, uninterrupted progress bars',
+      'Personalized tasks - just for you'
     ].rand
   end
 
