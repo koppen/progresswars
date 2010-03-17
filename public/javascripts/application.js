b70e64eb280eee2645c346164c6385e0899372ef
Jakob Skjerning <jakob@mentalized.net>
2010-03-18 00:10:55 +0100

    Moved all the perform task logic to the client side

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -25,7 +25,7 @@ function setExperienceProgress(percentage) {
 	setProgress('.experience .bar .progress', percentage);
 };
 
-function setLevel(level) {
+function updateLevel(level) {
 	var current_level = $(".level .value").text();
 	if (current_level != level) {
 		$(".level .value").text(level).hide('puff', {percent:500}, 1000).fadeIn();
@@ -39,3 +39,107 @@ function trackGoals() {
 	$('.link_to_twitter').click(function() { if (pageTracker) { pageTracker._trackPageview('/g/link_to_twitter'); }});
 };
 
+Task = function() { /*main object*/ };
+$.extend(Task, {
+	getValue: function(attributeName, defaultValue) {
+		var value = $.cookie(attributeName);
+		if (value) {
+			return value;
+		} else {
+			return defaultValue;
+		};
+	},
+
+	getIntValue: function(attributeName, defaultValue) {
+		return parseInt(Task.getValue(attributeName, defaultValue), 10);
+	},
+
+	setValue: function(attributeName, value) {
+		$.cookie(attributeName, value);
+	},
+
+	calculateExperienceForLevel: function(level) {
+		return Math.pow((level + 1), 2);
+	},
+
+	ding: function() {
+		// @task.level += 1
+		Task.level(Task.level() + 1);
+
+		// @task.experience -= @task.experience_for_next_level
+		Task.experience(Task.experience() - Task.experience_for_next_level());
+		
+		// @task.experience_for_next_level = @task.calculate_experience_for_next_level
+		Task.experience_for_next_level(Task.calculateExperienceForLevel(Task.level()));
+		
+		updateLevel(Task.level());
+	},
+	
+	clicks: function(newValue) {
+		if (newValue) {
+			return Task.setValue('clicks', newValue);
+		} else {
+			return Task.getIntValue('clicks', 0);
+		}
+	},
+	
+	clicks_to_complete: function(newValue) {
+		if (newValue) {
+			return Task.setValue('clicks_to_complete', newValue);
+		} else {
+			return Task.getIntValue('clicks_to_complete', 0);
+		}
+	},
+	
+	experience: function(newValue) {
+		if (newValue) {
+			return Task.setValue('experience', newValue);
+		} else {
+			return Task.getIntValue('experience', 0);
+		}
+	},
+	
+	experience_for_next_level: function(newValue) {
+		if (newValue) {
+			return Task.setValue('experience_for_next_level', newValue);
+		} else {
+			return Task.getIntValue('experience_for_next_level', 1);
+		}
+	},
+	
+	level: function(newValue) {
+		if (newValue) {
+			return Task.setValue('level', newValue);
+		} else {
+			return Task.getIntValue('level', 1);
+		}
+	},
+	
+	levelProgress: function() {
+		return Math.round(100.0 * Task.experience() / Task.experience_for_next_level())
+	},
+
+	progress: function() {
+		return Math.round(100.0 * Task.clicks() / Task.clicks_to_complete())
+		
+	}
+
+});
+
+function performTask() {
+	var clicks = Task.clicks();
+	Task.clicks(++clicks);
+
+	var experience = Task.experience();
+	Task.experience(experience + Task.level());
+
+	var progress = Task.progress();
+	setTaskProgress(progress);
+
+	if (Task.experience() >= Task.experience_for_next_level()) {
+		Task.ding()
+	};
+	setExperienceProgress(Task.levelProgress())
+
+	return false;
+}
